403aee7e9fc608cb07c8ab83fb35513e99950643
melissab147 <melissa.j.bica@gmail.com>
2015-03-18 11:40:00 -0600

    displaying image, caption, and user info for each post

--- public/scripts/frame.js
+++ public/scripts/frame.js
@@ -6,7 +6,7 @@ var MainFrame = React.createClass({
     },
     getData: function(data) {
         this.setState({data: data});
-        console.log(this.state.data);
+        console.log(this.state.data[0], "id: ",this.state.data[0].profile_owner_id);
     },
     confirmBullying: function() {
         console.log("Heard click")
@@ -24,14 +24,44 @@ var MainFrame = React.createClass({
         this.retrieveData();
     },
     render: function() {
-        return (
-            <div>
-                <button onClick={this.confirmBullying}>Yes</button>
-                <button onClick={this.denyBullying}>No</button>
-                {this.state.data.profile_owner_id}
+        if (this.state.data[0]){
+            console.log(this.state.data)
 
-            </div>            
-        )
+            var results = this.state.data;
+
+            // for (i = 0; i < 88; i++){
+            //     rows.push(<img src={this.state.data[i].image_url} align="left"></img>)
+            //     rows.push(<b>Owner:</b>)
+            //     rows.push({this.state.data[i].profile_owner_id})
+            //     rows.push(<b>Owner caption:</b> {this.state.data[i].owner_caption} )
+            //     rows.push(<b>Likes:</b> {this.state.data[i].likes})
+            // }
+            return (
+                <div>
+                    <button onClick={this.confirmBullying}>Yes</button>
+                    <button onClick={this.denyBullying}>No</button> <br/>
+                    <ol>
+                        {results.map(function(result) {
+                            return(
+                                <div>
+                                    <li key={result.id}><b>{result.profile_owner_id}</b>: {result.owner_caption}</li>
+                                    <img key={result.id} src={result.image_url} width="200"></img>
+                                    <p key={result.id}><b>Likes</b>: {result.likes}</p>
+                                    <p key={result.id}><b>Posted on</b>: {result.caption_time}</p>
+
+                                </div>
+                            );
+                        })}
+                    </ol>
+                </div>
+
+            )
+        }
+        else{
+            return (
+                <h2>No data</h2>
+            )
+        }
     }
 });
 
